54cd0646b790bb665c507e42f4a993b3ddc25726
Gitverde85 <rodrigo.verde@hotmail.com>
2026-04-30 09:58:40 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import openpyxl
+
+inv_file = openpyxl.load_workbook("inventory.xlsx")
+product_list = inv_file["Sheet1"]
+
+products_per_supplier = {}
+total_value_per_supplier = {}
+products_under_10_inv = {}
+
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
+    product_number = product_list.cell(product_row, 1).value
+    inventory_price = product_list.cell(product_row, 5)
+
+    # calculation number of products per supplier
+    if supplier_name in products_per_supplier:
+        current_num_products = products_per_supplier.get(supplier_name)
+        products_per_supplier[supplier_name] = current_num_products + 1
+    else:
+        products_per_supplier[supplier_name] = 1
+
+    #calculation total value of inventory per supplier
+    if supplier_name in total_value_per_supplier:
+        current_num_total_values = total_value_per_supplier.get(supplier_name)
+        total_value_per_supplier[supplier_name] = current_num_total_values + inventory * price
+    else:
+        total_value_per_supplier[supplier_name] = inventory * price
+
+    # logic products with inventory less than 10
+    if inventory < 10:
+        products_under_10_inv[int(product_number)] = int(inventory)
+
+    # add value for total inventory price
+    inventory_price.value = inventory * price
+
+inv_file.save("inventory_with_total_value.xlsx")
