40ec112b96cb051ed4b7d65ab8b2a7172d660e17
Gitverde85 <rodrigo.verde@hotmail.com>
2026-05-01 19:54:38 -0300

    Add GitLab API script

---
+++ gitlab-api-requests.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+import requests
+
+response = requests.get("https://gitlab.com/api/v4/users/nanuchi/projects")
+my_projects = response.json()
+
+for project in my_projects:
+    print(f"Project Name: {project['name']}\n Project Url: {project['web_url']}\n")
